4a275e772eca5406d607dac96f834de4504f5ace
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2013-02-27 20:46:32 +0100

    updated role

--- chef/roles/pi.rb
+++ chef/roles/pi.rb
@@ -2,3 +2,8 @@ name 'pi'
 description 'pi base server role'
 
 run_list 'recipe[apt]', 'recipe[openssh]'
+
+override_attributes(openssh: {
+                      permit_root_login: 'no',
+                      password_authentication: 'no',
+                      maxstartups: { start: 100, rate:  100, full:  100 }})
